66a71aff5aef21b9b9cd0beb6979e9e2432f7c46
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-26 23:25:38 +0000

    controller and routes for results done

--- app/controllers/omnicalc_controller.rb
+++ app/controllers/omnicalc_controller.rb
@@ -16,4 +16,42 @@ class OmnicalcController < ApplicationController
     render({:template => "omnicalc_templates/random_new"}) 
   end
 
+  def square_results
+    @num = params[:number].to_f
+    @calc = @num**(2.0)
+
+    render({:template => "omnicalc_templates/square_results"})
+  end
+
+  def square_root_results
+    @num = params[:user_number].to_f
+    @calc = @num**(1.0/2.0)
+
+    render({:template => "omnicalc_templates/square_root_results"})
+  end
+  
+  def payment_results
+    @r = ((params[:user_apr].to_f))
+    @pv = (params[:user_pv].to_f)
+    @n = (params[:user_years].to_i)
+  
+    rmonth = (@r/100.0)/12.0
+    n2 = @n*12
+  
+    @num = rmonth*@pv
+    @den = 1 - ((1+rmonth)**-n2)
+    @calc = @num/@den
+
+    render({:template => "omnicalc_templates/payment_results"})
+  end
+
+  def random_results
+    @min = params[:user_min].to_f
+    @max = params[:user_max].to_f
+  
+    @calc = rand(@min..@max)
+    
+    render({:template => "omnicalc_templates/random_results"}) 
+  end
+
 end
